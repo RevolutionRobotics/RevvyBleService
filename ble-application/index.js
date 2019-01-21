e01572870524f335532ab42f22ffb07a5a60f848
pavelaron <junkieman@freemail.hu>
2019-01-21 17:09:55 +0100

    Bleno lib implementation

---
+++ ble-application/index.js
@@ -0,0 +1,31 @@
+var bleno = require('bleno');
+
+var SystemInformationService = require('./systeminformationservice');
+
+var systemInformationService = new SystemInformationService();
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+
+    bleno.startAdvertising('Revvy BLE Node', [systemInformationService.uuid]);
+  }
+  else {
+
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('on -> advertisingStart: ' +
+    (error ? 'error ' + error : 'success')
+  );
+
+  if (!error) {
+
+    bleno.setServices([
+      systemInformationService
+    ]);
+  }
+});
---
+++ node_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+
+node -v
+npm -v
+
+echo DONE
