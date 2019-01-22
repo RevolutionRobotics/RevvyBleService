6a309eef6835babf90fc5edcc412f8822e62760c
pavelaron <junkieman@freemail.hu>
2019-01-22 14:58:29 +0100

    Added missing Blenocharacteristic class

--- ble-application/characteristics/uart.js
+++ ble-application/characteristics/uart.js
@@ -1,6 +1,8 @@
 var bleno = require('bleno');
 var util = require('util');
 
+var BlenoCharacteristic = bleno.Characteristic;
+
 var UartCharacteristic = function(port) {
  UartCharacteristic.super_.call(this, {
     uuid: '0000ffe1-0000-1000-8000-00805f9b34fb',
