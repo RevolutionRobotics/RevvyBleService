8735141c07f3ab3bb4371b907cc21e8070361b5a
raspberrypible3 <pi@raspberry.com>
2019-01-22 14:41:17 +0100

    Implemented serial communication

--- ble-application/characteristics/uart.js
+++ ble-application/characteristics/uart.js
@@ -1,8 +1,19 @@
 var bleno = require('bleno');
 var os = require('os');
 var util = require('util');
+var Readline = require('@serialport/parser-readline');
+var SerialPort = require('serialport');
 
 var BlenoCharacteristic = bleno.Characteristic;
+var port = new SerialPort('/dev/ttyS0', { 
+  baudRate: 115200,
+  parity: 'none',
+  stopBits: 1,
+  dataBits: 8
+});
+
+var parser = new Readline();
+port.pipe(parser);
 
 var UartCharacteristic = function() {
 
@@ -19,16 +30,15 @@ var UartCharacteristic = function() {
 };
 
 UartCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  console.log('EchoCharacteristic - onReadRequest: value = ' + this._value.toString('hex'));
+  callback(this.RESULT_SUCCESS, this._value);
+};
 
-  if(!offset) {
-    return false;
-  }
+UartCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
+  console.log(data);
+  port.write(data);
 
-  console.log('UartCharacteristic - onReadRequest: value = ' +
-    this._value.slice(offset, offset + bleno.mtu).toString()
-  );
-
-  callback(this.RESULT_SUCCESS, this._value.slice(offset, this._value.length));
+  callback(this.RESULT_SUCCESS, withoutResponse ? null : this._value);
 };
 
 util.inherits(UartCharacteristic, BlenoCharacteristic);
